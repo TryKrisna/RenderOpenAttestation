0aa63eee50f8c83523cc06ece694af73c9688df0
Try Krisna <trykrisnapersonal@gmail.com>
2023-02-13 15:42:56 +0700

    o

---
+++ src/templates/samples/customTemplateSample.ts
@@ -0,0 +1,62 @@
+// import { v2 } from "@govtechsg/open-attestation";
+
+// export interface CustomTemplateCertificate extends v2.OpenAttestationDocument {
+//   name: string;
+//   institute: string;
+//   foo?: {
+//     title: string;
+//   };
+//   $template: v2.TemplateObject;
+// }
+
+// export const customTemplateCertificate: CustomTemplateCertificate = {
+//   name: "John Doe",
+//   institute: "Institute of John Doe",
+//   issuers: [
+//     {
+//       name: "institute of blockchain"
+//     }
+//   ],
+//   $template: {
+//     name: "custom",
+//     type: v2.TemplateType.EmbeddedRenderer,
+//     url: "http://localhost:3000"
+//   },
+//   foo: {
+//     title: "Bar is awesome"
+//   }
+// };
+
+
+import { v2 } from "@govtechsg/open-attestation";
+
+export interface CocTemplateCertificate extends v2.OpenAttestationDocument {
+  name: string;
+  recipient: {
+    name: string;
+  };
+}
+
+export const cocTemplateCertificate: CocTemplateCertificate = {
+  name: "Digital Invoice Certificate",
+  issuers: [
+    {
+      name: "My name",
+      // 
+      documentStore: "0x9a89355De3f04Ad788ECb26B4e8B75805f23161D",
+      // documentStore: "0xBBb55Bd1D709955241CAaCb327A765e2b6D69c8b",
+      identityProof: {
+        location: "brown-teal-cattle.sandbox.openattestation.com",
+        type: v2.IdentityProofType.DNSTxt,
+      },
+    },
+  ],
+  recipient: {
+    name: "Krisna Try",
+  },
+  $template: {
+    name: "COC",
+    type: v2.TemplateType.EmbeddedRenderer,
+    url: "http://localhost:3000",
+  },
+};
